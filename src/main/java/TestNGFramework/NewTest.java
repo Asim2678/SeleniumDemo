b705f0ce65848e4848cc4ecfa14f2e83d160ab81
Asim2678 <asimbassra2002@yahoo.com>
2023-03-12 14:21:03 +0000

    Initial commit 12.03.2023

---
+++ src/main/java/TestNGFramework/NewTest.java
@@ -0,0 +1,13 @@
+package TestNGFramework;
+
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+    @Test
+    public void firstTest(){
+        System.out.println("This is my First Test");
+
+    }
+}
